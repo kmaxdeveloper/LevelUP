1aff3d4ef830598116f82b65d2c54c5d578a91e9
kmaxdeveloper <komilcoder7@gmail.com>
2023-03-19 18:56:39 +0500

    Fixed minor bugs

--- app/src/main/java/uz/kmax/levelup/fragment/GameFragment.kt
+++ app/src/main/java/uz/kmax/levelup/fragment/GameFragment.kt
@@ -1,26 +1,35 @@
 package uz.kmax.levelup.fragment
 
+import android.util.Log
 import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
+import com.google.android.gms.ads.rewarded.ServerSideVerificationOptions
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
 import uz.kmax.base.basefragment.BaseFragmentWC
 import uz.kmax.levelup.databinding.FragmentGameBinding
 import uz.kmax.levelup.dialog.DialogNext
 import uz.kmax.levelup.memory.SharedHelper
 
 class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::inflate) {
-    private var count : Int = 0
+    private var count: Int = 0
     private var maxCount = 100
-    private var level : Int = 1
-    private var step : Int = 1
+    private var level: Int = 1
+    private var step: Int = 1
     private var mInterstitialAd: InterstitialAd? = null
-    private var adRequest : AdRequest? = null
+    private var rewardedInterstitialAd: RewardedInterstitialAd? = null
+    private var adRequest: AdRequest? = null
     private var dialogNext = DialogNext()
-    private lateinit var shared : SharedHelper
+    private lateinit var shared: SharedHelper
+    private var rewardedAd: RewardedAd? = null
+    var isLoading: Boolean = true
+    var rewardedIsLoading = true
 
     override fun onViewCreated() {
         shared = SharedHelper(requireContext())
@@ -33,8 +42,10 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
 
         activity?.window?.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         MobileAds.initialize(requireContext()) {}
+
         val adView = AdView(requireContext())
         adRequest = AdRequest.Builder().build()
         adView.adUnitId = "ca-app-pub-4664801446868642/3547674791"
@@ -46,7 +57,7 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
         }
     }
 
-    private fun onClick(){
+    private fun onClick() {
         binding.countView.visibility = View.VISIBLE
         count += step
         binding.progressBar.progress = count
@@ -54,8 +65,8 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
         binding.countProgress.text = "$count"
     }
 
-    private fun check(){
-        if (count >= maxCount){
+    private fun check() {
+        if (count >= maxCount) {
             ++level
             count = 0
             binding.progressBar.progress = count
@@ -63,8 +74,31 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
             binding.countProgress.text = count.toString()
             dialogNext.show(requireContext())
             dialogNext.setOkListener {
-                if (mInterstitialAd != null){
-                    mInterstitialAd?.show(requireActivity())
+                if (!rewardedIsLoading) {
+                    rewardedInterstitialAd?.let { ad ->
+                        ad.show(
+                            requireActivity(),
+                            OnUserEarnedRewardListener {
+                                var type = it.type
+                                var amount = it.amount
+                            }
+                        )
+                    }
+                } else if (!isLoading) {
+                    rewardedAd?.let { ad ->
+                        ad.show(requireActivity(), OnUserEarnedRewardListener { rewardItem ->
+                            val rewardAmount = rewardItem.amount
+                            val rewardType = rewardItem.type
+                        })
+                    } ?: run {
+                        //Log.d(TAG, "The rewarded ad wasn't ready yet.")
+                    }
+                } else if (mInterstitialAd != null) {
+                    if (mInterstitialAd != null) {
+                        mInterstitialAd?.show(requireActivity())
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Ads Not Loaded !", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -72,48 +106,106 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
 
     override fun onResume() {
         super.onResume()
-        InterstitialAd.load(requireContext(),"ca-app-pub-4664801446868642/9346796384", adRequest!!, object : InterstitialAdLoadCallback() {
-            override fun onAdFailedToLoad(adError: LoadAdError) {
-                mInterstitialAd = null
-                //Toast.makeText(requireContext(), "Ad Not Loaded !", Toast.LENGTH_SHORT).show()
-            }
+        RewardedInterstitialAd.load(requireContext(), "ca-app-pub-3940256099942544/5354046379",
+            AdRequest.Builder().build(), object : RewardedInterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: RewardedInterstitialAd) {
+                    rewardedInterstitialAd = ad
+                    val options = ServerSideVerificationOptions.Builder()
+                        .setCustomData("SAMPLE_CUSTOM_DATA_STRING")
+                        .build()
+                    rewardedInterstitialAd!!.setServerSideVerificationOptions(options)
+                }
 
-            override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                mInterstitialAd = interstitialAd
-                //Toast.makeText(requireContext(), "Ad Loaded !", Toast.LENGTH_SHORT).show()
-            }
-        })
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    rewardedInterstitialAd = null
+                }
+            })
 
-        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+        InterstitialAd.load(requireContext(),
+            "ca-app-pub-4664801446868642/9346796384",
+            adRequest!!,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+        val adRequest = AdRequest.Builder().build()
+        RewardedAd.load(
+            requireContext(),
+            "ca-app-pub-4664801446868642/1030886732",
+            adRequest,
+            object : RewardedAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    rewardedAd = null
+                }
+
+                override fun onAdLoaded(ad: RewardedAd) {
+                    rewardedAd = ad
+                    isLoading = false
+                    val options = ServerSideVerificationOptions.Builder()
+                        .setCustomData("SAMPLE_CUSTOM_DATA_STRING")
+                        .build()
+                    rewardedAd!!.setServerSideVerificationOptions(options)
+                }
+            })
+
+        rewardedAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
             override fun onAdClicked() {
-                // Called when a click is recorded for an ad.
+                Toast.makeText(requireContext(), "Thank you !", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                rewardedAd = null
+                memoryLeak()
+            }
+
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) { rewardedAd = null }
+            override fun onAdImpression() {}
+            override fun onAdShowedFullScreenContent() {}
+        }
+
+        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdClicked() {
                 Toast.makeText(requireContext(), "Thank You !!!", Toast.LENGTH_SHORT).show()
             }
 
             override fun onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
                 mInterstitialAd = null
-                count = 0
-                binding.progressBar.progress = count
-                binding.countProgress.text = count.toString()
+                memoryLeak()
             }
 
             override fun onAdFailedToShowFullScreenContent(p0: AdError) {
-                // Called when ad fails to show.
                 mInterstitialAd = null
             }
 
-            override fun onAdImpression() {
-                // Called when an impression is recorded for an ad.
+            override fun onAdImpression() {}
+            override fun onAdShowedFullScreenContent() {}
+        }
 
+        rewardedInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdClicked() {
+                Toast.makeText(requireContext(), "Thank you !", Toast.LENGTH_SHORT).show()
             }
 
-            override fun onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-
+            override fun onAdDismissedFullScreenContent() {
+                memoryLeak()
+                rewardedInterstitialAd = null
             }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                rewardedInterstitialAd = null
+            }
+
+            override fun onAdImpression() {}
+            override fun onAdShowedFullScreenContent() {}
         }
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -126,4 +218,11 @@ class GameFragment : BaseFragmentWC<FragmentGameBinding>(FragmentGameBinding::in
         shared.setLeveCount(level)
         shared.setLeveViewCount(count)
     }
+
+    fun memoryLeak() {
+        count = 0
+        binding.progressBar.progress = count
+        binding.countProgress.text = count.toString()
+    }
+
 }
